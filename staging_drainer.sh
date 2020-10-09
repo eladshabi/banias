b2dc14e6616cde91f8346cd905b56ec3c33a3261
eladshabi <elads.eladsh@gmail.com>
2020-10-09 17:25:12 +0300

    Added Staging drainer - the fix of the "drain.sh" with outside parameters

--- staging_drainer.sh
+++ staging_drainer.sh
@@ -5,14 +5,14 @@ project_id=$2
 
 df_jobs=($(gcloud dataflow jobs list --filter="baniaspipeline" --status="active" --region="$df_working_region" --project="$project_id" --format='value(JOB_ID)'))
 
-echo "job found: ${#df_jobs[@]}"
+echo "Jobs found: ${#df_jobs[@]}"
 
 if [ ${#df_jobs[@]} -gt 1 ]; then
   echo "Found more then one active Dataflow job, please cancel or drain irrelevant jobs and try again."
   exit 1
 
 elif [ ${#df_jobs[@]} -eq 0 ]; then
-  echo "There are no active job has been found, please run new dataflow job."
+  echo "No active job has been found, please run new dataflow job."
   exit 1
 fi
 
@@ -35,6 +35,6 @@ while :
         echo "Job id: ${job_id} found with status: ""${job_status}"", the drain process has been completed."
         break
       fi
-      echo "Job id: ${job_id} found with status: ""${job_status}"". Waiting for the job to be drained, next check in 10 seconds."
+      echo "Job id: ${job_id} found with status: ""${job_status}"", waiting for the job to be drained, next check in 10 seconds."
       sleep 10
  done
